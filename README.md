ef068937807d6b0ebdbc08f11edf04f39cbc3119
ricardolupo <rlupo@getchef.com>
2016-11-09 16:26:03 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ms_testing
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'ms_testing'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures ms_testing'
+long_description 'Installs/Configures ms_testing'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/ms_testing/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/ms_testing' if respond_to?(:source_url)
